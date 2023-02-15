3aa33abe92081ddee3147e2eb4ef980df7fda7c2
Andrew Ealovega <Andrew@Ealovega.dev>
2023-02-14 20:03:27 -0500

    Add support for gz-garden This should also have support for fortress, but need to test.

--- phoenix_gazebo/launch/include/gazebo/gazebo.launch.py
+++ phoenix_gazebo/launch/include/gazebo/gazebo.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     pkg_phoenix_description = get_package_share_directory(
         'phoenix_description')
     pkg_phoenix_gazebo = get_package_share_directory('phoenix_gazebo')
-    pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
+    pkg_ros_ign_gazebo = get_package_share_directory('ros_gz_sim')
 
     gazebo_world = LaunchConfiguration(
         'gazebo_world', default='purdue_gp_track.sdf')
@@ -26,25 +26,25 @@ def generate_launch_description():
     ign_gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_ign_gazebo, 'launch',
-                         'ign_gazebo.launch.py')),
+                         'gz_sim.launch.py')),
         launch_arguments={
-            'ign_args': launch.substitutions.PathJoinSubstitution([pkg_phoenix_gazebo + '/worlds/', gazebo_world])
+            'gz_args': launch.substitutions.PathJoinSubstitution([pkg_phoenix_gazebo + '/worlds/', gazebo_world])
         }.items()
     )
 
     ign_bridge = Node(
-        package='ros_ign_bridge',
+        package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/world/phoenix_world/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
-            '/model/phoenix/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
-            '/model/phoenix/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
-            '/model/phoenix/odometry@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
-            '/model/phoenix/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
-            '/mid_rgbd_camera/image@sensor_msgs/msg/Image[ignition.msgs.Image',
-            '/mid_rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
-            '/sky_rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
-            '/sky_rgbd_camera/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/world/phoenix_world/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/model/phoenix/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+            '/model/phoenix/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
+            '/model/phoenix/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+            '/model/phoenix/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
+            '/mid_rgbd_camera/image@sensor_msgs/msg/Image[gz.msgs.Image',
+            '/mid_rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            '/sky_rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            '/sky_rgbd_camera/image@sensor_msgs/msg/Image[gz.msgs.Image',
         ],
         output='screen',
         remappings=[
@@ -60,7 +60,7 @@ def generate_launch_description():
             ('/sky_rgbd_camera/camera_info', '/camera/score/camera_info'),
         ])
         
-    ign_spawn_robot = Node(package='ros_ign_gazebo',
+    ign_spawn_robot = Node(package='ros_gz_sim',
                            executable='create',
                            arguments=[
                                '-name', 'phoenix', '-x', '0', '-z', '0', '-Y',
