c0a25b49a00098e1cc365f1675bc2d4016c2ad54
Andrew Ealovega <Andrew@Ealovega.dev>
2023-10-11 03:12:52 -0400

    Properly integrate Oakd into TF Also fixes gazebo frames (thanks joe)

--- phoenix_robot/launch/include/oakd/oakd.launch.py
+++ phoenix_robot/launch/include/oakd/oakd.launch.py
@@ -2,61 +2,46 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
-from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.actions import ComposableNodeContainer, Node, SetRemap
 from launch_ros.descriptions import ComposableNode
 
 
-def launch_setup(context, *args, **kwargs):
-    log_level = 'info'
-    if (context.environment.get('DEPTHAI_DEBUG') == '1'):
-        log_level = 'debug'
-
-    params_file = LaunchConfiguration("params_file")
-
-    name = LaunchConfiguration('name').perform(context)
-
-    return [
-        ComposableNodeContainer(
-            name=name + "_container",
-            namespace="",
-            package="rclcpp_components",
-            executable="component_container",
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="depthai_ros_driver",
-                    plugin="depthai_ros_driver::Camera",
-                    name=name,
-                    parameters=[params_file],
-                    remappings=[
-                        ('/oak/stereo/image_raw', '/camera/mid/depth'),
-                        ('/oak/stereo/camera_info', '/camera/mid/depth/camera_info'),
-                        ('/oak/stereo/image_raw/compressed', '/camera/mid/depth/compressed'),
-                        ('/oak/rgb/image_raw', '/camera/mid/rgb'),
-                        ('/oak/rgb/image_raw/compressed', '/camera/mid/rgb/compressed'),
-                        ('/oak/rgb/image_raw/theora', '/camera/mid/rgb/compressed/theora'),
-                        ('/oak/rgb/camera_info', '/camera/mid/rgb/camera_info'),
-                        ('/oak/imu/data', '/camera/mid/imu')]
-                )
-            ],
-            arguments=['--ros-args', '--log-level', log_level],
-            output="both",
-        )
-    ]
-
-
+# Launches the oak-d driver, as well as its tf publisher
 def generate_launch_description():
-    pkg_phnx_robot = get_package_share_directory("phoenix_robot")
+    oakd_package = get_package_share_directory("depthai_ros_driver")
+    phnx_package = get_package_share_directory("phoenix_robot")
+
+    camera = GroupAction(
+        actions=[
+            # Topic remaps
+            SetRemap('/oak/stereo/image_raw', '/camera/mid/depth'),
+            SetRemap('/oak/stereo/camera_info', '/camera/mid/depth/camera_info'),
+            SetRemap('/oak/stereo/image_raw/compressed', '/camera/mid/depth/compressed'),
+            SetRemap('/oak/rgb/image_raw', '/camera/mid/rgb'),
+            SetRemap('/oak/rgb/image_raw/compressed', '/camera/mid/rgb/compressed'),
+            SetRemap('/oak/rgb/image_raw/theora', '/camera/mid/rgb/compressed/theora'),
+            SetRemap('/oak/rgb/camera_info', '/camera/mid/rgb/camera_info'),
+            SetRemap('/oak/imu/data', '/camera/mid/imu'),
+            SetRemap('/robot_description', '/camera/mid/robot_description'),
+
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(oakd_package, 'launch', 'camera.launch.py')),
+                launch_arguments={
+                    'params_file': os.path.join(phnx_package, 'config', 'oakd', 'camera.yaml'),
+                    'camera_model': 'OAK-D-S2'
+                }.items()
+            )]
+    )
 
     declared_arguments = [
         DeclareLaunchArgument("name", default_value="oak"),
-        DeclareLaunchArgument("params_file",
-                              default_value=os.path.join(pkg_phnx_robot, 'config', 'oakd', 'camera.yaml')),
     ]
 
-    return LaunchDescription(
-        declared_arguments + [OpaqueFunction(function=launch_setup)]
-    )
+    return LaunchDescription([
+        camera,
+    ])
