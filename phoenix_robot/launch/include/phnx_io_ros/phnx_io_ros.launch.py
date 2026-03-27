06fe882f743283ee00ffc0ab356e813ec18454ed
Elijah Stickel <estickel@umich.edu>
2026-03-27 10:32:50 -0400

    Config: I added the config and updated the phnx_io_ros.launch.py to work with the config, vefified build and run in robot

--- phoenix_robot/launch/include/phnx_io_ros/phnx_io_ros.launch.py
+++ phoenix_robot/launch/include/phnx_io_ros/phnx_io_ros.launch.py
@@ -1,22 +1,23 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    pir = Node(package='phnx_io_ros',
-               executable='phnx_io_ros',
-               parameters=[{
-               }]
-               )
+    # This just tells the script where your new folder is
+    config = os.path.join(
+        get_package_share_directory('phoenix_robot'),
+        'config', 'phoenix_io_ros', 'phoenix_io_ros.yaml'
+    )
 
     return LaunchDescription([
-        # Launch Arguments
-        pir
+        Node(
+            package='phnx_io_ros',
+            executable='phnx_io_ros',
+            name='phnx_io_ros',
+            parameters=[config],
+            output='screen'
+        )
     ])
